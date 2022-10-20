c316d90365f4c461b28a2abb8d402dc6ddcb620e
Anthony Pham <akpham95@gmail.com>
2022-10-20 14:24:23 -0500

    helloAlert fix

--- public/main.js
+++ public/main.js
@@ -1,8 +1,8 @@
-let contactButton = document.querySelectorAll('button.button-10')
+let contactButton = document.querySelector(".button-10")
 
 const helloAlert = evt => {
 
     alert("Alert on click")
 }
 
-contactButton.addEventListener('click', helloAlert)
+contactButton.addEventListener("click", helloAlert)
